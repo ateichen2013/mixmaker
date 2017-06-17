006117dd1cf5101e794e3e3bc25ff6b7b57214cc
mmmfrieddough <mmmfrieddough@gmail.com>
2017-06-16 20:04:27 -0400

    Sliders kind of working

--- Web Code/scripts/sliders.js
+++ Web Code/scripts/sliders.js
@@ -1,37 +1,39 @@
-var sliders = $("#sliders .slider");
+$(window).load(function(){
+    var sliders = $("#sliders .slider");
 
-sliders.each(function() {
-    var value = parseInt($(this).text(), 10),
-        availableTotal = 400;
+    sliders.each(function() {
+        var value = parseInt($(this).text(), 10),
+            availableTotal = 100;
 
-    $(this).empty().slider({
-        value: 0,
-        min: 0,
-        max: 400,
-        range: "max",
-        step: 10,
-        animate: 100,
-        slide: function(event, ui) {
+        $(this).empty().slider({
+            value: 0,
+            min: 0,
+            max: 100,
+            range: "max",
+            step: 1,
+            animate: 100,
+            slide: function(event, ui) {
 
-            // Get current total
-            var total = 0;    
-            
-            sliders.not(this).each(function() {
-                total += $(this).slider("option", "value");
-            });    
-            
+                // Get current total
+                var total = 0;    
 
-            var max = availableTotal - total;            
-                
-            if (max - ui.value >= 0) {
-                // Need to do this because apparently jQ UI
-                // does not update value until this event completes
-                total += ui.value;
-                console.log(max-ui.value);
-                $(this).siblings().text(ui.value);
-            } else {
-                return false;
+                sliders.not(this).each(function() {
+                    total += $(this).slider("option", "value");
+                });    
+
+
+                var max = availableTotal - total;            
+
+                if (max - ui.value >= 0) {
+                    // Need to do this because apparently jQ UI
+                    // does not update value until this event completes
+                    total += ui.value;
+                    console.log(max-ui.value);
+                    $(this).siblings().text(ui.value);
+                } else {
+                    return false;
+                }
             }
-        }
+        });
     });
 });
