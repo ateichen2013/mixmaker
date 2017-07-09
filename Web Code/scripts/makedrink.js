74d238c4ca1a12a457e186885d93ac3500ff69f5
mmmfrieddough <mmmfrieddough@gmail.com>
2017-07-09 12:08:50 -0400

    New way of sending drink data

--- Web Code/scripts/makedrink.js
+++ Web Code/scripts/makedrink.js
@@ -1,28 +1,26 @@
-$(document).ready(function() {
-	$('.MakeDrink').click(function() {
-		console.log("submitted");
-		$.ajax({
-			url: 'http://10.10.0.29:80/makedrink',
-			type: 'post',
-			data: {
-				drink1: $('#dr1').text(),
-				drink2: $('#dr2').text(),
-				drink3: $('#dr3').text(),
-				drink4: $('#dr4').text()
-			},
-			headers: {
+function MakeDrink(dr1, dr2, dr3, dr4) {
+    console.log("submitted");
+    $.ajax({
+        url: 'http://10.10.0.29:80/makedrink',
+        type: 'post',
+        data: {
+            drink1: dr1,
+            drink2: dr2,
+            drink3: dr3,
+            drink4: dr4
+        },
+        headers: {
 
-			},
-			dataType: 'text',
-			success: function(data) {
-                console.log(data)
-				if(data == "success!"){
-                    alert("Drink poured successfully")
-                }
-                else if(data == "failure!"){
-                    alert("Please insert cup")
-                }
-			}
-		});
-	});
-});
+        },
+        dataType: 'text',
+        success: function(data) {
+            console.log(data)
+            if(data == "success!"){
+                alert("Drink poured successfully")
+            }
+            else if(data == "failure!"){
+                alert("Please insert cup")
+            }
+        }
+    });
+}
