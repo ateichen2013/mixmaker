b37db367d01e6a122b23dcb8ed223ff5e7d078bb
mmmfrieddough <mmmfrieddough@gmail.com>
2017-07-23 12:10:17 -0400

    Database connect test

--- Web Code/PHP/db_connect.php
+++ Web Code/PHP/db_connect.php
@@ -13,8 +13,6 @@ $dbpass = "ed2mixmaker"; // needs to be changed to reflect LAMP server credentia
 
 $db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-echo "testing";
-
 if($db->connect_errno > 0)
 {
     die('Unable to connect to database [' . $db->connect_error . ']');
