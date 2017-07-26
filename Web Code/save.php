f9ebe190412e9104253b17659d043d51d237ff7c
mmmfrieddough <mmmfrieddough@gmail.com>
2017-07-26 13:30:27 -0400

    Updated database insertion

--- Web Code/save.php
+++ Web Code/save.php
@@ -1,23 +1,13 @@
 <?php
 include("db_connect.php");
 
-if (isset($_REQUEST['drinkname'])) {
-	/* Prepared statement, stage 1: prepare query */
-	if (!($stmt = $db->prepare("INSERT INTO drinks(drinkname, drink1, drink2, drink3, drink4) VALUES (?, ?, ?, ?, ?, ?)")))
-	{
-		echo "Prepare failed: (" . $db->errno . ") " . $db->error;
-	}
-
-	/* Prepared statement, stage 2: bind parameters*/
-	if (!$stmt->bind_param('sssss', $_POST["drinkname"], $_POST["drink1"], $_POST["drink2"], $_POST["drink3"], $_POST["drink4"]))
-	{
-		echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
-
-	/* Prepared statement, stage 3: execute*/
-	if (!$stmt->execute())
-	{
-		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
+if( $_POST )
+	$sql = "INSERT INTO drinks (drinkname, drink1, drink2, drink3, drink4) VALUES ('" . $_POST["drinkname"] . "', '" . $_POST["drink1"] . "', '" . $_POST["drink2"] . "', '" . $_POST["drink3"] . "', '" . $_POST["drink4"] . "')";
+    
+    if ($db->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 ?>
