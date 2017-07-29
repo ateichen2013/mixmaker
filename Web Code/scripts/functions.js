7e863b3713e6fa6f03fd4b593dc0664a5bd2087d
mmmfrieddough <mmmfrieddough@gmail.com>
2017-07-28 23:13:31 -0400

    Test

--- Web Code/scripts/functions.js
+++ Web Code/scripts/functions.js
@@ -1,7 +1,7 @@
 $(window).load(function() {
     console.log("loaded");
     
-    $('.saveslider').enhanceWithin();
+    $('*').enhanceWithin();
     
     sliders = $("#sliders .slider");
 	$(".slider").on("change", function(event, ui) {
